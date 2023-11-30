8d03e723e16cd6f93b66018aa16c1f85f532ea02
GustavoMuraroGonzales <gustavo.gonzales.tec@gmail.com>
2023-11-30 16:59:40 -0300

    Adicionada conteudo tipos de saida

---
+++ js/index.js
@@ -0,0 +1,5 @@
+document.getElementById("texto").innerHTML="Meu Primeiro Texto <b>JS</b>. Usando InnerHTML";
+
+document.write ("Meu primeiro texto com document.write");
+
+console.log("Oi isso é um console.log")
